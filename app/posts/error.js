d8b6ff878a34e878aaee0eb5b4e344d23e5ee141
ssup_sam <sarl.2001@hotmail.com>
2025-02-03 21:58:11 -0300

    chore: practicing routes

---
+++ app/posts/error.js
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { useState } from "react";
+
+const error = ({ error }) => {
+  return <>{error}</>;
+};
+
+export default error;
---
+++ app/posts/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <>Post page</>;
+};
+
+export default page;
