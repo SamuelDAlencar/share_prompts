aab6d71430411c0df40d48f5b0c5419615ce48bf
ssup_sam <sarl.2001@hotmail.com>
2025-02-17 23:39:07 -0300

    feat: implements profile page with posts from profile

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,23 @@
+import { connectToDB } from "@utils/database";
+
+import Prompt from "@models/prompt";
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const { id } = await params;
+
+    const prompts = await Prompt.find({
+      creator: id,
+    }).populate("creator");
+
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(error, {
+      status: 500,
+    });
+  }
+};
